490299a5de1e93fa924261491a4e5bc69ad80b43
Trevor Pelton <tnpflyer20@msn.com>
2016-10-12 11:05:35 -0600

    intitial files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<h1>Persona: Primarily Young adults who are pretty heavy internet users looking for easy and cheap (free) entertainment.</h1>
-		<h2>Use case: User looking for entertainment can log in, view images that interest them, and comment/discuss the image</h2>
-		<h3>Entities: The image itself, the user profile, and the comments about it.</h3>
+		<h2>Use case: User looking for entertainment can log in, view images that interest them, and comment/discuss the image.</h2>
+		<h3>Entities: The image itself, the user profile, and the comments about it. </h3>
 	</body>
 </html>
