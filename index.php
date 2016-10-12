9780f8a9cf88c256f480cd2a7852be58360753d5
Trevor Pelton <tnpflyer20@msn.com>
2016-10-12 11:01:30 -0600

    intitial files

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Data Design for Imgur
+		</title>
+	</head>
+	<body>
+		<h1>Persona: Primarily Young adults who are pretty heavy internet users.</h1>
+		<h2>Use case: User looking for entertainment can log in, view images that interest them, and comment/discuss the image</h2>
+		<h3>Entities: The image itself, the user profile, and the comments about it.</h3>
+	</body>
+</html>
