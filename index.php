7167f3ebb8631956ed1ccd4ba262c2a881406877
Trevor Pelton <tnpflyer20@msn.com>
2016-10-12 14:15:26 -0600

    Content 1

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <html>
 	<head>
+		<meta charset="UTF-8">
 		<title>Data Design for Imgur
 		</title>
 	</head>
 	<body>
-		<h2>Persona: Primarily Young adults who are pretty heavy internet users looking for easy and cheap (free) entertainment. These users should have a fair bit of free time, and use the site mostly as a distraction</h2>
-		<h2>Use case: User looking for entertainment can log in, view images that interest them, and comment/discuss/ or upvote/downvote the image. Or alternatively submit content for these things to happen to their own pictures. </h2>
+		<h2>Persona: Joe Smith Age: 19 Occupation: College student Uses the site during free time and boring lectures to ease boredom. Should be accessable by both laptops/PCs and mobile devices like phones and tablets. </h2>
+		<h2>Use case: Primarily used for easing boredom and easy shallow entertainment. User looking for entertainment can log in, view images that interest them, and comment/discuss/ or upvote/downvote the image. Or alternatively submit content for these things to happen to their own pictures. </h2>
 		<ul>
 			<li>User sees an photo that interests them</li>
 			<li>User views comments and either adds to them, or rates the comment up and down.</li>
