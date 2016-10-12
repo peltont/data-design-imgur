b67497d2987c5fe1ac44dda8da46cf04be4e2292
Trevor Pelton <tnpflyer20@msn.com>
2016-10-12 11:26:41 -0600

    intitial files

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
 		</title>
 	</head>
 	<body>
-		<h1>Persona: Primarily Young adults who are pretty heavy internet users looking for easy and cheap (free) entertainment.</h1>
-		<h2>Use case: User looking for entertainment can log in, view images that interest them, and comment/discuss the image.</h2>
+		<h1>Persona: Primarily Young adults who are pretty heavy internet users looking for easy and cheap (free) entertainment. These users should have a fair bit of free time, and use the site mostly as a distraction</h1>
+		<h2>Use case: User looking for entertainment can log in, view images that interest them, and comment/discuss/ or upvote/downvote the image. Or alternatively submit content for these things to happen to their own pictures. </h2>
+		<ul>
+			<li>User sees an photo that interests them</li>
+			<li>User views comments and either adds to them, or rates the comment up and down.</li>
+		</ul>
 		<h3>Entities: The image itself, the user profile, and the comments about it. </h3>
 		<h4>   fjavsdfS</h4>
 	</body>
