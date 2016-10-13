a4bf743f3e8a7f7916b7bb283b8727757e502863
Trevor Pelton <tnpflyer20@msn.com>
2016-10-13 13:40:56 -0600

    header added

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
 	</head>
 	<body>
 		<h1 class="center">Data Design Project</h1>
-		<h4>Persona: Name: Joe Smith<br>
-			Age: 19<br>
-			Occupation: College student<br>
+		<br />
+		<h4>Persona: Name: Joe Smith<br />
+			Age: 19<br />
+			Occupation: College student<br />
 			Uses the site during free time and boring lectures to ease boredom. Should be accessable by both laptops/PCs and mobile devices like phones and tablets. </h4>
 		<h4>Use case: Primarily used for easing boredom and easy shallow entertainment. User looking for entertainment can log in, browse for images that interest them, and comment/discuss/ or upvote/downvote the image. Or alternatively submit content for these things to happen to their own pictures. </h4>
 		<ul>
