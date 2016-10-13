e6a8338d8dc19a915b2cbf02da4b4c095b26f23e
Trevor Pelton <tnpflyer20@msn.com>
2016-10-13 08:18:06 -0600

    Content 1 initial data

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
 		</title>
 	</head>
 	<body>
-		<h2>Persona: Joe Smith Age: 19 Occupation: College student Uses the site during free time and boring lectures to ease boredom. Should be accessable by both laptops/PCs and mobile devices like phones and tablets. </h2>
-		<h2>Use case: Primarily used for easing boredom and easy shallow entertainment. User looking for entertainment can log in, view images that interest them, and comment/discuss/ or upvote/downvote the image. Or alternatively submit content for these things to happen to their own pictures. </h2>
+		<h2>Persona: Name: Joe Smith
+			Age: 19
+			Occupation: College student
+			Uses the site during free time and boring lectures to ease boredom. Should be accessable by both laptops/PCs and mobile devices like phones and tablets. </h2>
+		<h2>Use case: Primarily used for easing boredom and easy shallow entertainment. User looking for entertainment can log in, browse for images that interest them, and comment/discuss/ or upvote/downvote the image. Or alternatively submit content for these things to happen to their own pictures. </h2>
 		<ul>
 			<li>User sees an photo that interests them</li>
 			<li>User views comments and either adds to them, or rates the comment up and down.</li>
@@ -20,5 +23,8 @@
 			</ul>
 			</li>
 		</ul>
+		<ul>Entities
+		<li>The image</li>
+		<li>Comments<li>Score (upvote/downvote)</li></ul>
 	</body>
 </html>
