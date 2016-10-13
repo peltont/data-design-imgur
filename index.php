16b5ed6cbab4e973bf0266e926c4defeade761a9
Trevor Pelton <tnpflyer20@msn.com>
2016-10-13 13:27:02 -0600

    more styling

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 		</title>
 	</head>
 	<body>
-		<h2>Persona: Name: Joe Smith
-			Age: 19
-			Occupation: College student
+		<h2>Persona: Name: Joe Smith<br>
+			Age: 19<br>
+			Occupation: College student<br>
 			Uses the site during free time and boring lectures to ease boredom. Should be accessable by both laptops/PCs and mobile devices like phones and tablets. </h2>
 		<h2>Use case: Primarily used for easing boredom and easy shallow entertainment. User looking for entertainment can log in, browse for images that interest them, and comment/discuss/ or upvote/downvote the image. Or alternatively submit content for these things to happen to their own pictures. </h2>
 		<ul>
